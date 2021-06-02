ee9a12445094e22ab75e26ebf0461530b2ac157b
Bibek Lekhak <bibek@computingcastle.com>
2021-06-02 06:49:47 +0545

    Write first failing test inside tests folder

---
+++ tests/PatroAPITest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PatroAPI\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class PatroAPITest extends TestCase
+{
+    public function testMyTest(): void
+    {
+        $this->assertTrue(false);
+    }
+}
