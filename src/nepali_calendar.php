778f51dac172efc9f93ff068a69cdc05d6b891dc
Bibek Lekhak <bibek@computingcastle.com>
2021-06-02 07:04:52 +0545

    Create test case out of commented-out example

--- src/nepali_calendar.php
+++ src/nepali_calendar.php
@@ -564,8 +564,3 @@ class NepaliCalendar
 		}
 	}
 }
-
-//  Example:
-//	$cal = new Nepali_Calendar();
-//	print_r ($cal->eng_to_nep(2008,11,23));
-//	print_r($cal->nep_to_eng(2065,8,8));
--- tests/PatroAPITest.php
+++ tests/PatroAPITest.php
@@ -7,4 +7,12 @@ use PHPUnit\Framework\TestCase;
 class PatroAPITest extends TestCase
 {
     
+    public function testingTwoDateConversions(): void
+    {
+        //  Example:
+        $cal = new \PatroAPI\NepaliCalendar();
+        $this->assertSame($cal->eng_to_nep(2008, 11, 23), ['year' => 2065, 'month' => 8, 'date' => 8, 'day' => 'Sunday', 'nmonth' => 'Mangshir', 'num_day' => 1]);
+        $this->assertSame($cal->nep_to_eng(2065, 8, 8), ['year' => 2008, 'month' => 11, 'date' => 23, 'day' => 'Sunday', 'emonth' => '', 'num_day' => 1, 'nmonth' => 'November']);
+    }
+    
 }
