a3dd0e851ab710635ed383aacdbc75d43e662cf9
Bibek Lekhak <bibek@computingcastle.com>
2021-06-03 10:23:07 +0545

    Test is_leap_year()

--- tests/PatroAPITest.php
+++ tests/PatroAPITest.php
@@ -64,6 +64,15 @@ class PatroAPITest extends TestCase
         $this->assertSame($this->Cal->_get_nepali_month(12), "Chaitra");
     }
     
+    public function testIsLeapYear(): void
+    {
+        $listOfLeapYearsBetween1900And2020 = [1904, 1908, 1912, 1916, 1920, 1924, 1928, 1932, 1936, 1940, 1944, 1948, 1952, 1956, 1960, 1964, 1968, 1972, 1976, 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020];
+        
+        foreach ($listOfLeapYearsBetween1900And2020 as $year) {
+            $this->assertTrue( $this->Cal->is_leap_year($year) );
+        }
+    }
+    
     protected function tearDown(): void
     {
         $this->Cal = null;
